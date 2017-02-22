0dcbeeee64d74be7ea4400fe8a7d06aef72a1e7e
carlos <carlos.xell.3@gmail.com>
2017-02-22 12:34:00 -0300

    adição de arquivo gulp e suas configurações, adição de imagens iniciais removidas de imagem JPG/PNG de 'layout', adição de pasta CSS com estrutura para BEM/SMACSS, adição de arquivo JS com funções iniciais

---
+++ assets/js/functions.js
@@ -0,0 +1,78 @@
+//funções iniciais
+var getId = function (el) {
+        return document.getElementById(el);
+    },
+    detectIe = (function () {
+        //classe que detecta navegadores IE em geral
+        window.navigator.userAgent.match('Trident') ? true : false;
+    })(),
+    getAll = function(el){
+        var item = document.querySelectorAll(el),
+            itemArray = [].slice.call(item);
+
+        return itemArray;
+    },
+    getElem = function (elem) {
+        return document.querySelector(elem);
+    },
+    addClass = function (el, className) {
+        return el.classList.add(className);
+    },
+    removeClass = function (el, className) {
+        el.classList.remove(className);
+    },
+    toggleClass = function (el, className) {
+        el.classList.toggle(className);
+    },
+    getIeBrowser = function (target) {
+        navigator.userAgent.match(target) ? addClass(getElem('html'), 'IE-9') : console.info('Not IE9 browser');
+    },
+    request = (function () {
+        var xhr = new XMLHttpRequest();
+
+        function stateChange() {
+            if (xhr.readyState === 4) {
+                // 4 = "loaded"
+                if (xhr.status === 200) {
+                    // 200 = OK
+                    // ...our code here...
+                    return xhr.response;
+                } else {
+                    console.error('Erro de requisição');
+                }
+            }
+        }
+
+        return{
+            get: function (method, url, async, setHeader) {
+                xhr.open(method, url, async);
+                if(setHeader) {
+                    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                }
+                xhr.onreadystatechange = stateChange();
+                xhr.send();
+                return xhr.responseText;
+            },
+            post: function (method, url, async, data, setHeader) {
+                xhr.open(method, url, async);
+                if(setHeader) {
+                    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                }
+                xhr.onreadystatechange = stateChange();
+                xhr.send(data);
+                return xhr.responseText;
+            }
+        };
+    })();
+
+//Verificação se é o browser é IE9
+getIeBrowser('MSIE 9.0');
+
+
+//chamadas de funções
+var init = function () {};
+
+//função que chama todas as outras
+document.addEventListener('DOMContentLoaded', function () {
+    init();
+});
