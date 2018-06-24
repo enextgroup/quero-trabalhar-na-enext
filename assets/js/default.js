31170b0f5901ec5622951e9288e686d5acc6bc3e
williansCMathias <williansconstantinom@gmail.com>
2018-06-23 21:15:02 -0300

    array funcionando

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,91 +1,24 @@
 $(document).ready(function(){
 
-var json =
-{
-  "potions": {
-    "1": {
-      "id": 1,
-      "name": "Aging Potion",
-      "image": "aging-potion.png",
-      "price": 29.99,
-      "effect": "Causes the drinker to advance in age",
-      "ingredients": [
-        "Red Wine",
-        "Prune Juice",
-        "Hairy Fungus",
-        "Tortoise Shell",
-        "Caterpillar",
-        "Bat Tongue"
-      ]
-    },
-    "2": {
-      "id": 2,
-      "name": "Bulgeye Potion",
-      "image": "bulgeye-potion.png",
-      "price": 19.99,
-      "effect": "It affects one's eyes, causing them to swell",
-      "ingredients": [
-        "Beetle eyes",
-        "Eel eyes"
-      ]
-    },
-    "3": {
-      "id": 3,
-      "name": "Dragon Tonic",
-      "image": "dragon-tonic.png",
-      "price": 64.99,
-      "effect": "A tonic used to cure sick dragons",
-      "ingredients": [
-        "Eagle Owl feathers",
-        "Peacock feathers",
-        "Giant Purple Toad warts"
-      ]
-    },
-    "4": {
-      "id": 4,
-      "name": "Love Potion",
-      "image": "love-potion.png",
-      "price": 29.99,
-      "effect": "The one who drinks it falls deeply in love with the first person they see",
-      "ingredients": [
-        "Petals from a red rose",
-        "Essence of violet",
-        "Canary flight and down feathers",
-        "Newt eyes"
-      ]
-    },
-    "5": {
-      "id": 5,
-      "name": "Polyjuice Potion",
-      "image": "polyjuice-potion.png",
-      "price": 99.99,
-      "effect": "Allows the drinker to assume the form of someone else",
-      "ingredients": [
-        "Lacewing flies",
-        "Leeches",
-        "Powdered bicorn horn",
-        "Knotgrass",
-        "Fluxweed",
-        "Shredded Boomslang skin"
-      ]
-    },
-    "6": {
-      "id": 6,
-      "name": "Sleeping Draught",
-      "image": "sleeping-draught.png",
-      "price": 29.99,
-      "effect": "Causes the drinker to fall almost instantaneously into a deep but temporary sleep",
-      "ingredients": [
-        "Sprigs of Lavender",
-        "Measures of Standard Ingredient",
-        "Blobs of Flobberworm Mucus",
-        "Valerian sprigs"
-      ]
-    }
+/*for (var key in vpotions.potions) {
+      var lista = vpotions.potions[key];
+      console.log(lista);
   }
-};
+*/
+var arr = Object.keys(vpotions.potions).map(function(k) { 
+  return vpotions.potions[k];
+  var nome = vpotions.potions[k].name;
+});
 
-var count = Object.keys(json.potions).length;
+
+console.log(arr);
+//var count = Object.keys(vpotions.potions).length;
+var i = "";
+  for (i = 0; i <= arr.length; i++){
+    var x = arr.nome[i];
+    console.log(x);
+  }
+/*var count = Object.keys(json.potions).length;
 //console.log(count);
 for (i = 0; i <= count; i++){
   for (var key in json) {
@@ -93,7 +26,14 @@ for (i = 0; i <= count; i++){
       console.log(lista);
   }
 }
-
+*/
+/*var count = Object.keys(vpotions.potions).length;
+var i = "";
+  for (i = 0; i <= count; i++){
+    var x = vpotions.potions[i];
+    console.log(x);
+  }
+*/
 //document.getElementById("teste").innerHTML = "<div>"+id+"</div><div>"+nome+"</div>";
 
 // Pega botão com id "btn"
